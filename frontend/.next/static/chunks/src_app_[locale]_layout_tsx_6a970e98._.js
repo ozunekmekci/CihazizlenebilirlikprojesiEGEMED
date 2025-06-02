2f63bdec9c9a0c98a39cad4e78836ffadcdb0987
ozunekmekci <60329732+ozunekmekci@users.noreply.github.com>
2025-06-02 15:08:18 +0300

    fix: node_modules ve .next git geçmişinden çıkarıldı, .gitignore güncellendi

--- frontend/.next/static/chunks/src_app_[locale]_layout_tsx_6a970e98._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_361a9de7._.js",
-  "static/chunks/src_83d2f757._.js",
-  "static/chunks/src_styles_global_99ee5e55.css"
-],
-    source: "dynamic"
-});
